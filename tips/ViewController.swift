54ab4691fe5a84b4f95484583485a0f4c09e1185
Gabriel Such <gabrielsuch@gmail.com>
2015-04-13 18:08:23 -0700

    Initial Project

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -10,16 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmount: UITextField!
+    
+    @IBOutlet weak var tip: UISegmentedControl!
+   
+    @IBOutlet weak var split: UISegmentedControl!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    func defaultTip() -> Int {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        return defaults.integerForKey("default_tip")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        totalLabel.text = "$0.00"
+        tip.selectedSegmentIndex = defaultTip()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func onEditingChange(sender: AnyObject) {
+        totalLabel.text = NSString(format:"$%.2f", total())
+    }
+    
+    func amountValue() -> Double  {
+        return NSString(string: billAmount.text).doubleValue
+    }
+    
+    func tipValue() -> Double {
+        let tipPercentages = [0.18, 0.2, 0.22]
+        let selected = tipPercentages[tip.selectedSegmentIndex]
+        return amountValue() * selected
+    }
+    
+    func splitValue() -> Int {
+        return split.selectedSegmentIndex + 1
+    }
+    
+    func total() -> Double {
+        return (amountValue() + tipValue()) / Double(splitValue())
+    }
+    
 }
 
